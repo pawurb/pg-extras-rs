372c419a0d0618f3e0e645455af069986b05dae2
Gokhan Demir <yadazula@gmail.com>
2024-10-26 23:22:27 +0300

    feat: add null indexes check to diagnostics

--- src/diagnose.rs
+++ src/diagnose.rs
@@ -1,10 +1,12 @@
 use crate::size_parser::to_bytes;
-use crate::{cache_hit, extensions, ssl_used, unused_indexes, Extensions, PgExtrasError};
+use crate::{cache_hit, extensions, null_indexes, ssl_used, unused_indexes, Extensions, PgExtrasError};
 use sqlx::types::BigDecimal;
 
 const TABLE_CACHE_HIT_MIN: f32 = 0.985;
 const INDEX_CACHE_HIT_MIN: f32 = 0.985;
 const UNUSED_INDEXES_MIN_SIZE_BYTES: u64 = 1_000_000; // 1 MB
+const NULL_INDEXES_MIN_SIZE_MB: &str = "1"; // 1 MB
+const NULL_MIN_NULL_FRAC_PERCENT: f64 = 50.0; // 50%
 
 #[derive(Debug)]
 enum Check {
@@ -71,6 +73,7 @@ impl Diagnose {
             Check::TableCacheHit => Self::table_cache_hit().await,
             Check::IndexCacheHit => Self::index_cache_hit().await,
             Check::UnusedIndexes => Self::unused_index().await,
+            Check::NullIndexes => Self::null_index().await,
             Check::SslUsed => Self::ssl_used().await,
             _ => Ok(CheckResult::new(true, "Not implemented".to_string(), stringify!(check).to_string())),
         }
@@ -140,5 +143,26 @@ impl Diagnose {
             .join(",\n");
 
         Ok(CheckResult::new(false, format!("Unused indexes detected:\n{}", print_indexes), stringify!(unused_indexes).to_string()))
+    }
+
+    async fn null_index() -> Result<CheckResult, PgExtrasError> {
+        let indexes = null_indexes(Some(NULL_INDEXES_MIN_SIZE_MB.to_string())).await?
+            .into_iter()
+            .filter(|i|
+                i.null_frac.trim_end_matches('%')
+                .parse::<f64>()
+                .unwrap_or(0.0) >= NULL_MIN_NULL_FRAC_PERCENT)
+            .collect::<Vec<_>>();
+
+        if indexes.is_empty() {
+            return Ok(CheckResult::new(true, "No null indexes detected.".to_string(), stringify!(null_indexes).to_string()))
+        }
+
+        let print_indexes = indexes.iter()
+            .map(|i| format!("'{}' size {} null values fraction {}", i.index, i.index_size, i.null_frac))
+            .collect::<Vec<_>>()
+            .join(",\n");
+
+        Ok(CheckResult::new(false, format!("Null indexes detected:\n{}", print_indexes), stringify!(unused_indexes).to_string()))
     }
 }
